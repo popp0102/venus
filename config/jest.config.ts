48b1245ed6a50e5dd6490bc6572e26ac1acb900c
Jason Poppler <popp0102@gmail.com>
2025-03-30 13:57:13 -0600

    Initial commit to venus

---
+++ config/jest.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from '@jest/types';
+import path from 'path';
+
+const config: Config.InitialOptions = {
+  roots: [path.resolve(__dirname, '../tests')],
+  rootDir: path.resolve(__dirname, '../'),
+  testEnvironment: 'jsdom',
+  testMatch: ["<rootDir>/tests/**/*.test.(tsx|ts)"],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: path.resolve(__dirname, './tsconfig.json'),
+    }],
+  },
+  setupFilesAfterEnv: [path.resolve(__dirname, '../tests/jest.setup.ts')],
+  moduleNameMapper: {
+    '^.+\\\\.(css|less)$': '<rootDir>/tests/__mocks__/CSS_Stub.ts',
+    '^@lib/(.*)$': '<rootDir>/lib/$1',
+    '^@util/(.*)$': '<rootDir>/lib/util/$1',
+    '^@components/(.*)$': '<rootDir>/lib/components/$1',
+    '^@hooks/(.*)$': '<rootDir>/lib/hooks/$1'
+  },
+};
+export default config;
---
+++ lib/components/Venus.tsx
@@ -0,0 +1,8 @@
+export default function Venus() {
+  return (
+    <>
+      <h1>New Package Created</h1>
+      <h2>Venus ready to go!</h2>
+    </>
+  )
+}
