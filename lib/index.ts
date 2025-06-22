f3ab1c2f07180b0c9f86a2e16f14972dc631aaac
Jason Poppler <popp0102@gmail.com>
2025-06-22 16:58:28 -0600

    fix

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,6 @@
 import CosmicText from "./components/Cosmic/Text/CosmicText";
 import CosmicTitle from "./components/Cosmic/Title/CosmicTitle";
-import CosmicButton from "./components/Cosmic/Title/CosmicButton";
+import CosmicButton from "./components/Cosmic/Button/CosmicButton";
 import HamburgerButton from "./components/HamburgerButton/HamburgerButton";
 
 export {
