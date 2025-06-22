03579f08eacc5f2c5c9f55356e815277c484acb4
Jason Poppler <popp0102@gmail.com>
2025-06-22 16:42:02 -0600

    add Cosmic Text

---
+++ lib/components/Cosmic/Title/CosmicTitle.jsx
@@ -0,0 +1,11 @@
+import { CosmicText } from '../Text/CosmicText';
+
+import './CosmicTitle.css';
+
+export default function CosmicTitle({ children }) {
+  return (
+    <CosmicText className="cosmic-title" text={children} randomColor/>
+  );
+}
+
+
--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,9 @@
 import CosmicText from "./components/Cosmic/Text/CosmicText";
+import CosmicTitle from "./components/Cosmic/Title/CosmicTitle";
 import HamburgerButton from "./components/HamburgerButton/HamburgerButton";
 
 export {
   CosmicText,
+  CosmicTitle,
   HamburgerButton,
 };
